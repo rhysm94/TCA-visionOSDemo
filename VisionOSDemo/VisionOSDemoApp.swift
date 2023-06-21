1dff0ec29d0cd51fc208584c7e476849f848a7f5
Rhys Morgan <rhysm@me.com>
2023-06-21 23:07:33 +0100

    Initial Commit

---
+++ VisionOSDemo/VisionOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSDemoApp.swift
+//  VisionOSDemo
+//
+//  Created by Rhys Morgan on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
